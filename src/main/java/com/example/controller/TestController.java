1c1c2a20039a92199ef4f3eef19284be94d13e59
QQUINA <1099685836@qq.com>
2017-06-20 14:42:02 +0800

    qwe12

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -1,7 +1,11 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by Administrator on 2017/6/20.
  */
+@Controller
 public class TestController {
+
 }
